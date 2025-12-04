4a101e387e192439cbe5f5af6be0c0ba9dea0c48
Dylan Montgomery <147453930+dylcmonty@users.noreply.github.com>
2025-12-04 17:25:07 -0500

    Create oeuvre.js

---
+++ srv/webapps/clients/fruitfulnetworkdevelopment.com/frontend/oeuvre/oeuvre.js
@@ -0,0 +1,5 @@
+// oeuvre/oeuvre.js
+// Oeuvre-specific initialization logic
+
+// Oeuvre component doesn't need specific initialization beyond data injection
+// Data injection is handled by data_injection.js
