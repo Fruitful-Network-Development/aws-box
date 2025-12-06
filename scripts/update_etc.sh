574780416bb8b9733879082d481b228371067ea0
Dylan Montgomery <147453930+dylcmonty@users.noreply.github.com>
2025-12-06 16:29:54 -0500

    Create update_etc.sh

---
+++ scripts/update_etc.sh
@@ -0,0 +1,13 @@
+# pull latest from GitHub
+#!/bin/bash
+set -euo pipefail
+
+PROJECT_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
+cd "$PROJECT_ROOT"
+
+git pull --ff-only
+
+# Grant execute permissions to the script:
+  # chmod +x update_etc.sh
+# Add the script's directory to the system's PATH environment variable by adding the following line to your shell's configuration file:
+  # export PATH=$PATH:/aws/aws/GH-aws/scripts
