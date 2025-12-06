02c1a337c21ab4c3013343f0e04d016bd93362a9
Dylan Montgomery <147453930+dylcmonty@users.noreply.github.com>
2025-12-06 14:22:18 -0500

    Delete srv/webapps directory

--- srv/webapps/clients/fruitfulnetworkdevelopment.com/frontend/anthology/anthology.js
+++
@@ -1,5 +0,0 @@
-// anthology/anthology.js
-// Anthology-specific initialization logic
-
-// Anthology grid rendering and click handlers are handled by data_injection.js
-// This file is a placeholder for future anthology-specific functionality
--- srv/webapps/clients/fruitfulnetworkdevelopment.com/frontend/oeuvre/oeuvre.js
+++
@@ -1,5 +0,0 @@
-// oeuvre/oeuvre.js
-// Oeuvre-specific initialization logic
-
-// Oeuvre component doesn't need specific initialization beyond data injection
-// Data injection is handled by data_injection.js
